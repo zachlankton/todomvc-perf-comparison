fbe62d0c4e0a76cbd52c406287d08726f8953879
Leo Horie <lhorie@klick.com>
2014-08-17 12:13:56 -0400

    fix remaining count

--- todomvc-benchmark/todomvc/mithril/js/views/footer-view.js
+++ todomvc-benchmark/todomvc/mithril/js/views/footer-view.js
@@ -1,17 +1,18 @@
 app.footer = function(ctrl) {
-	var count = ctrl.list.countCompleted()
+	var completed = ctrl.list.countCompleted()
+	var remaining = ctrl.list.items.length - completed
 	return m('footer#footer', [
 		m('span#todo-count', [
-			m('strong', ctrl.list.items.length), ' item' + (ctrl.list.items.length > 1 ? 's' : '') + ' left'
+			m('strong', remaining), ' item' + (remaining > 1 ? 's' : '') + ' left'
 		]),
 		m('ul#filters', [
 			m('li', m('a[href=/]', app.selectedIf(ctrl.filter, ''), 'All')),
 			m('li', m('a[href=/active]', app.selectedIf(ctrl.filter, 'active'), 'Active')),
 			m('li', m('a[href=/completed]', app.selectedIf(ctrl.filter, 'completed'), 'Completed'))
 		]),
-		count == 0 ? '' : m('button#clear-completed', {
+		completed == 0 ? '' : m('button#clear-completed', {
 			onclick: ctrl.list.clearCompleted
-		}, 'Clear completed (' + count + ')')
+		}, 'Clear completed (' + completed + ')')
 	]);
 }
 app.selectedIf = function(filter, value) {

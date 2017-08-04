d539ddce13c2ca98a7fb643e5c6ffc201189b031
zachlankton <zachlankton@gmail.com>
2017-08-04 14:30:29 -0400

    added DOM Data Mirror to Test Suite

--- todomvc-benchmark/resources/tests.js
+++ todomvc-benchmark/resources/tests.js
@@ -2,6 +2,43 @@ var numberOfItemsToAdd = 50;
 var Suites = [];
 
 Suites.push({
+    name: 'DOM Data Mirror',
+    url: 'todomvc/domDataMirror/index.html',
+    version: '0.1.21',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var inputEvent = document.createEvent('Event');
+                inputEvent.initEvent('input', true, true);
+                newTodo.value = 'dData ------- Something to do ' + i;
+                newTodo.dispatchEvent(inputEvent);
+
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keypress', true, true);
+                keydownEvent.keyCode = 13; // VK_ENTER
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = deleteButtons.length - 1; i > -1; i--)
+                deleteButtons[i].click();
+        })
+    ]
+});
+
+Suites.push({
     name: 'Mithril',
     url: 'todomvc/mithril/index.html',
     version: '0.1.21',
